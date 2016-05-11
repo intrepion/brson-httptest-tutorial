e19068d40156bbb9ea9f0c414fe561699c636ae3
Oliver Forral <intrepion@gmail.com>
2016-05-10 20:24:35 -0700

    2. Serving a struct as JSON

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 extern crate iron;
+extern crate rustc_serialize;
 
 use iron::prelude::*;
 use iron::status;
+use rustc_serialize::json;
+
+#[derive(RustcEncodable)]
+struct Greeting {
+    msg: String
+}
 
 fn main() {
     fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
+        let greeting = Greeting { msg: "Hello, World".to_string() };
+        let payload = json::encode(&greeting).unwrap();
+        Ok(Response::with((status::Ok, payload)))
     }
 
     Iron::new(hello_world).http("localhost:3000").unwrap();
